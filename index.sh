52b14390ec2f678634bb9f44fc46f7145731c0ea
Prathima-R <prathimaprati504@gmail.com>
2025-09-15 15:36:52 +0530

    triggers index.sh

--- index.sh
+++ index.sh
@@ -1,2 +1,5 @@
 echo "my fiest demo"
 echo "hello tis is uding by triggers" 
+echo "it doesnt take any clone poli scm is if you chnage any cide in github that time inly triggers the buikd other wikes nor"
+echo "if you ci=ompare others bild prridiacly it takes clone you can set up even throuh there is  no code chagnes evary minute it is created build and you can give numbers ogf build you can stored or delate"
+
