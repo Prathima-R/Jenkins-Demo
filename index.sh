a198235cf27e7748babb875b3406ec032293e707
Prathima-R <prathimaprati504@gmail.com>
2025-09-15 15:58:24 +0530

    Update index.sh

--- index.sh
+++ index.sh
@@ -4,3 +4,4 @@ echo "it doesnt take any clone poli scm is if you chnage any cide in github that
 echo "if you ci=ompare others bild prridiacly it takes clone you can set up even throuh there is  no code chagnes evary minute it is created build and you can give numbers ogf build you can stored or delate"
 
 echo "hello"
+echo "hi"
