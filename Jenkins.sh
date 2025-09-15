fa69030327c64bffe78e365ef41e48bcc43925ca
Prathima-R <prathimaprati504@gmail.com>
2025-09-15 12:07:03 +0530

    Update Jenkins.sh

--- Jenkins.sh
+++ Jenkins.sh
@@ -1,14 +1,22 @@
 #!/bin/bash
 sudo apt update -y
-sudo apt install fontconfig openjdk-21-jre -y
+sudo apt install -y fontconfig openjdk-21-jre
 
+# Add Jenkins GPG key
+sudo mkdir -p /etc/apt/keyrings
 sudo wget -O /etc/apt/keyrings/jenkins-keyring.asc \
 https://pkg.jenkins.io/debian-stable/jenkins.io-2023.key
-echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc]" \
-https://pkg.jenkins.io/debian-stable binary/ | sudo tee \
-/etc/apt/sources.list.d/jenkins.list > /dev/null
-sudo apt-get update
-sudo apt-get install jenkins
 
+# Add Jenkins repo
+echo "deb [signed-by=/etc/apt/keyrings/jenkins-keyring.asc] \
+https://pkg.jenkins.io/debian-stable binary/" | sudo tee \
+/etc/apt/sources.list.d/jenkins.list > /dev/null
+
+# Update and install Jenkins
+sudo apt-get update -y
+sudo apt-get install -y jenkins
+
+# Enable and start Jenkins
 sudo systemctl enable jenkins
-sudo systemctl status Jenkins
+sudo systemctl start jenkins
+sudo systemctl status jenkins --no-pager
