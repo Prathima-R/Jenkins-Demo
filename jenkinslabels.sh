9c440a1eb4e360c9b104a8bc1e5c9a2315098d7f
Prathima-R <prathimaprati504@gmail.com>
2025-09-17 19:55:05 +0530

    Create jenkins labels code.sh

---
+++ jenkinslabels.sh
@@ -0,0 +1,28 @@
+pipeline {
+    agent { label 'Prod || Preprod' }   // will run on Prod or Preprod node
+
+    tools {
+        maven 'maven'        // name must match Maven tool configured in Jenkins
+        jdk 'java-11'        // name must match JDK configured in Jenkins
+    }
+
+    stages {
+        stage('Git Checkout') {
+            steps {
+                git branch: 'dev', url: 'https://github.com/Prathima-R/Jenkins-pipeline.git'
+            }
+        }
+
+        stage('Compile') {
+            steps {
+                sh "mvn compile"
+            }
+        }
+
+        stage('Build') {
+            steps {
+                sh "mvn package"
+            }
+        }
+    }
+}
